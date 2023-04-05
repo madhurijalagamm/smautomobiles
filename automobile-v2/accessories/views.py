3dafb531c6aec72e80358ac5fafdbd1702f124c3
madhurijalagamm <116717596+madhurijalagamm@users.noreply.github.com>
2023-04-05 22:20:56 +0530

    Add files via upload

---
+++ automobile-v2/accessories/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout, update_session_auth_hash 
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm, PasswordChangeForm
+from django.contrib import messages 
+
+
+def accessoriesaction(request):
+    return render(request, 'accessories.html')
